dc0c706776c65e709d48c95c665bb6bef4362851
tangjl <jordanltang@gmail.com>
2017-03-09 02:10:21 -0500

    fixed ordering and added more cards

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'app/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -49,7 +49,7 @@ def about_blog(request):
 
 @login_required
 def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    posts = Post.objects.filter(published_date__isnull=True).order_by('-created_date')
     return render(request, 'app/post_draft_list.html', {'posts': posts})
     
 @login_required
